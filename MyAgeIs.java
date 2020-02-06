602bedf7a5edcddc951067a033e375644f4b58a2
valdezk <54403612+valdezk@users.noreply.github.com>
2020-02-06 13:33:40 -0500

    added MyAgeIs

---
+++ MyAgeIs.java
@@ -0,0 +1,15 @@
+
+public class MyAgeIs 
+{
+        public static void main (String []args)
+  { 
+      int currentYear = 2020;
+      int yearOfBirth = 1998;
+      
+      int myAge = currentYear - yearOfBirth; 
+      
+      System.out.print("My age is:" + myAge );
+      
+  }
+    
+}
